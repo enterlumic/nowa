dd559d17786b5063afc8147eb97ccc06d728f379
Gus <gustavo.gnu87@gmail.com>
2024-07-09 20:10:50 -0600

    mejoras textos

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -32,7 +32,7 @@
 
                     <!-- Start::header-element -->
                     <div class="main-header-center ms-3 d-sm-none d-md-none d-lg-block form-group">
-                        <input class="form-control" placeholder="Search..." type="search">
+                        <input class="form-control" placeholder="Buscar..." type="search">
                         <button class="btn"><i class="fas fa-search"></i></button>  
                     </div>  
                     <!-- End::header-element -->
@@ -269,7 +269,7 @@
                     <!-- End::header-element -->
 
                     <!-- Start::header-element -->
-                    <div class="header-element notifications-dropdown">
+                    <div class="header-element notifications-dropdown" >
                         <!-- Start::header-link|dropdown-toggle -->
                         <a href="javascript:void(0);" class="header-link dropdown-toggle" data-bs-toggle="dropdown" data-bs-auto-close="outside" id="messageDropdown" aria-expanded="false">
                             <svg xmlns="http://www.w3.org/2000/svg" class="header-link-icon" width="24" height="24" viewBox="0 0 24 24"><path d="M19 13.586V10c0-3.217-2.185-5.927-5.145-6.742C13.562 2.52 12.846 2 12 2s-1.562.52-1.855 1.258C7.185 4.074 5 6.783 5 10v3.586l-1.707 1.707A.996.996 0 0 0 3 16v2a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-2a.996.996 0 0 0-.293-.707L19 13.586zM19 17H5v-.586l1.707-1.707A.996.996 0 0 0 7 14v-4c0-2.757 2.243-5 5-5s5 2.243 5 5v4c0 .266.105.52.293.707L19 16.414V17zm-7 5a2.98 2.98 0 0 0 2.818-2H9.182A2.98 2.98 0 0 0 12 22z"/></svg>
